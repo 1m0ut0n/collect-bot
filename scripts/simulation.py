ac4f2a8f2be605a4f2b88d739a86c4f031607ff3
gaspard.beunier <77894810+1m0ut0n@users.noreply.github.com>
2025-01-27 10:15:21 +0100

    first commit

---
+++ scripts/simulation.py
@@ -0,0 +1,17 @@
+"""File that contains the simulation parameters and some helpful functions to calculate some states during simulation."""
+
+cylinders = {
+    1: {
+        'mass': 1.0,
+        'value': 1.0,
+    },
+    2: {
+        'mass': 2.0,
+        'value': 2.0,
+    },
+    3: {
+        'mass': 2.0,
+        'value': 3.0,
+    },
+}
+
