e4890a67194efeee7e2ee9fe2927a878b648f648
gaspard.beunier <77894810+1m0ut0n@users.noreply.github.com>
2025-01-27 11:25:11 +0100

    add simulation params

--- scripts/simulation.py
+++ scripts/simulation.py
@@ -1,5 +1,7 @@
 """File that contains the simulation parameters and some helpful functions to calculate some states during simulation."""
+import math
 
+# Cylinder mass and values
 cylinders = {
     1: {
         'mass': 1.0,
@@ -15,3 +17,33 @@ cylinders = {
     },
 }
 
+# Comsumption parameters
+comsumptionParams = {
+    'b': 1e-2, 
+    'b0': 1e-2
+}
+initialFuelQuantity = 1e4
+
+# Speed parameters
+speedParams = {
+    'alpha': 0.0698,
+    'V0': 1.0 # m/s
+}
+
+# Function to get the speed of the robot for a certain mass
+def speed(mass):
+    return speedParams.V0 * math.exp(-speedParams.alpha * mass)
+
+# Function to get the consumption of the robot for a certain mass
+def consumption(mass):
+    return comsumptionParams.b * mass + comsumptionParams.b0
+
+
+# Function to get the cost in fuel of a certain distance at a certain speed
+def cost(distance, mass):
+    return consumption(mass) * distance
+
+
+# Function to get the cost in time of a certain distance at a certain speed
+def timeCost(distance, mass):
+    return distance / speed(mass)
